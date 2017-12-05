b8bd1b98ad13238344e14f4dea34a19894f72425
Josh Oettinger <oettingerj@carleton.edu>
2017-12-05 13:25:19 -0600

    Initial Commit

---
+++ UserDefaults-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefaults-Demo
+//
+//  Created by Josh Oettinger on 12/5/17.
+//  Copyright © 2017 Josh Oettinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
